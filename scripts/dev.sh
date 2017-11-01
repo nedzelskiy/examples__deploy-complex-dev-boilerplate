a42a6cca8e684f4a9ca137d4214ac3de3b2acfc6
Pavel Nedzelskiy <pavel.nedzelskiy@dukascopy.com>
2017-11-01 18:55:16 +0200

    upgrade dev.sh

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 eval `grep "^export " ./scripts/env.sh` && \
-node node_modules/concurrently/src/main.js  \
+node node_modules/concurrently/src/main.js  --prefix "[{index}]-pid:[{pid}]"  --allow-restart  --restart-tries 10 \
 "node node_modules/webpack/bin/webpack.js --config ./configs/webpack-client.conf.js" \
 "node microservices/server-app-restarter.js" \
 "node microservices/server-static-watcher.js" \
