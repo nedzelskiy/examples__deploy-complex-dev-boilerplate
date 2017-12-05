aaba4c21e6c672a14643875f32de916fa20bdcb4
Pavel Nedzelskiy <pavel.nedzelskiy@dukascopy.com>
2017-12-05 10:53:17 +0200

    deleted build

--- build/assets/server-js.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-var div = document.querySelector('.server-js');
-div.innerHTML = "Js from server: It works!";
--- build/modules/makeResponseText.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = () => {
-    return 'Server render: It works!';
-};
-//# sourceMappingURL=makeResponseText.js.map
