f6116962456988fad1facd75df3a98b270c41c5f
Pavel Nedzelskiy <pavel.nedzelskiy@dukascopy.com>
2017-12-08 17:43:13 +0200

    [3758b1] upgraded microservices

--- server.js
+++ server.js
@@ -2,10 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const SERVER__PORT = process.env.SERVER__PORT || process.env.PORT || 80;
 const SERVER__URL = process.env.SERVER__URL || `http://localhost:${SERVER__PORT}`;
-const http = require("http");
+const http_1 = require("http");
 const handleHttp_1 = require("./handleHttp");
-const server = http.createServer();
-const DEBUG = (process.env.NODE_ENV === 'production') ? false : true;
+const server = http_1.createServer();
 server.on('request', handleHttp_1.default).listen(SERVER__PORT, () => {
     console.log(`Server is running on ${SERVER__URL} ${new Date()}`);
 });
